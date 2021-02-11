561c7f38d769d9f143d1b60d1616b3232123139a
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-02-11 22:30:07 +0500

    finally it's working

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,6 @@
 import time
 import subprocess
-# from os import startfile
 
-# from psutil import process_iter
-import psutil
 import shogi.KIF
 
 import rab_with_db as rwd
@@ -27,15 +24,8 @@ print(date)
 print(conditions)
 ################################################################################################
 
-cmd = 'C:/Users/Olga/Downloads/Shogidokoro_with_engine/Kristallweizen/Kristallweizen-wcsc29-avx2'
-# a = startfile(cmd)
-
-# time.sleep(2)
-# print(a)
-
-# for proc in process_iter():
-#     if proc.name() == cmd.split("/")[-1]:
-#         proc.kill()
+# cmd = 'C:/Users/Olga/Downloads/Shogidokoro_with_engine/Kristallweizen/Kristallweizen-wcsc29-avx2'
+cmd = "C:/Users/Olga/Downloads/LesserkaiSrc/LesserkaiSrc/x64/Debug/Lesserkai"
 
 process = subprocess.Popen(cmd, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -47,94 +37,9 @@ while line_usi := process.stdout.readline().decode('utf8'):
         print(line_usi, end='')
         break
 
-# # option
-process.stdin.write('setoption name WriteDebugLog value true\r\n'.encode())
-process.stdin.flush()
-# process.stdin.write('setoption name USI_Ponder value true\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name USI_Hash value 256\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name Threads value 4\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name Hash value 16\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name MultiPV value 1\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name SkillLevel value 20\r\n'.encode())
-# process.stdin.flush()
-# # process.stdin.write('setoption name WriteDebugLog value false\r\n'.encode())
-# # process.stdin.flush()
-# process.stdin.write('setoption name NetworkDelay value 120\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name NetworkDelay2 value 1120\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name MinimumThinkingTime value 2000\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name SlowMover value 100\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name MaxMovesToDraw value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name DepthLimit value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name NodesLimit value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name Contempt value 2\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name ContemptFromBlack value false\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name EnteringKingRule value CSARule27\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name EvalDir value eval\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name NarrowBook value false\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookMoves value 16\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookIgnoreRate value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookFile value standard_book.db\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookDir value book\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookEvalDiff value 30\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookEvalBlackLimit value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookEvalWhiteLimit value -140\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookDepthLimit value 16\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name BookOnTheFly value false\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name ConsiderBookMoveCount value false\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name PvInterval value 300\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name ResignValue value 99999\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name nodestime value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name Param1 value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name Param2 value 0\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name ConsiderationMode value false\r\n'.encode())
-# process.stdin.flush()
-# process.stdin.write('setoption name OutputFailLHPV value true\r\n'.encode())
-# process.stdin.flush()
-
-# This is used to synchronize the engine with the GUI
-process.stdin.write('isready\r\n'.encode())
-process.stdin.flush()
-# while process.stdout.
-time.sleep(2)
-temp_line = process.stdout.readline().decode('utf8')
-while temp_line != 'readyok\r\n':
-    time.sleep(2)
-    print(temp_line)
-    temp_line = process.stdout.readline().decode('utf8')
-print('isready end')
-# process.stdout.flush()
+# # # option
+# process.stdin.write('setoption name WriteDebugLog value true\r\n'.encode())
+# process.stdin.flush()  
 
 # This is sent to the engine when the next search (started with position and go) will be from a different game.
 process.stdin.write('usinewgame\r\n'.encode())
@@ -144,21 +49,26 @@ print('usinewgame end')
 # Set up the position described in sfenstring on the internal board and play the moves on the internal board.
 for i in range(len(kif['moves'])):
     temp_str = 'position startpos moves ' + ' '.join(kif['moves'][:i+1]) + '\r\n'
-    print(temp_str)
+    print(temp_str[:-2], " ход........ ", end="")
     process.stdin.write(('position startpos moves ' + ' '.join(kif['moves'][:i+1]) + '\r\n').encode())
-    # process.stdin.flush()
-    print('pos end')
+    process.stdin.flush()
     process.stdin.write('go infinite\r\n'.encode())
-    print('go end')
-    # process.stdin.flush()
+    process.stdin.flush()
     time.sleep(10)
     process.stdin.write('stop\r\n'.encode())
-    print('stop end')
     process.stdin.flush()
+    out_variants = {}
     while line_end := process.stdout.readline().decode('utf8'):
-        print(line_end)
         if line_end.find('bestmove') > -1:
-            print(line_end, end='')
+            print(line_end.replace('\r\n',''), end='')
+            print('///',out_variants[line_end.split(' ')[-1].replace('\r\n','')])
             break
-
+        else:
+            temp = line_end.split(' ')
+            if temp[1] == 'time':
+                # либо считать среднее, либо выбирать наибольшее
+                if 'depth' in temp:
+                    out_variants[temp[11].replace('\r\n','')] = temp[9]
+                else:
+                    out_variants[temp[11].replace('\r\n','')] = temp[7]
 process.kill()

17a61cf3587442a9751addb58d75a86673cf033e
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-02-19 10:30:56 +0500

    gikou2 с проблемами на cp и searchmoves

--- data_processing.py
+++ data_processing.py
@@ -25,7 +25,8 @@ print(conditions)
 ################################################################################################
 
 # cmd = 'C:/Users/Olga/Downloads/Shogidokoro_with_engine/Kristallweizen/Kristallweizen-wcsc29-avx2'
-cmd = "C:/Users/Olga/Downloads/LesserkaiSrc/LesserkaiSrc/x64/Debug/Lesserkai"
+# cmd = "C:/Users/Olga/Downloads/LesserkaiSrc/LesserkaiSrc/x64/Debug/Lesserkai"
+cmd = "C:/Users/Olga/Downloads/gikou2_win/gikou"
 
 process = subprocess.Popen(cmd, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -34,6 +35,14 @@ process.stdin.write('usi\r\n'.encode())
 process.stdin.flush()
 while line_usi := process.stdout.readline().decode('utf8'):
     if line_usi == 'usiok\r\n':
+        print(line_usi, end='')
+        break
+
+# This is used to synchronize the engine with the GUI
+process.stdin.write('isready\r\n'.encode())
+process.stdin.flush()
+while line_usi := process.stdout.readline().decode('utf8'):
+    if line_usi == 'readyok\r\n':
         print(line_usi, end='')
         break
 
@@ -50,10 +59,10 @@ start_pos = ""
 for i in range(len(kif['moves'])):
     # найти cp за текущий ход
     temp_str = 'position startpos moves ' + start_pos + ' ' + kif['moves'][i] + '\r\n'
-    print(temp_str[:-2], " реальный ход........ ", end="")
+    print(temp_str[:-2], " поиск cp за реальный ход........ ", end="")
     process.stdin.write((temp_str).encode())
     process.stdin.flush()
-    process.stdin.write(("go infinite searchmoves " + kif['moves'][i] + '\r\n').encode()) #      :(
+    process.stdin.write(("go infinite searchmoves " + kif['moves'][i] + '\r\n').encode())
     process.stdin.flush()
     time.sleep(10)
     process.stdin.write('stop\r\n'.encode())
@@ -65,19 +74,24 @@ for i in range(len(kif['moves'])):
             break
         else:
             temp = line_end.split(' ')
-            if temp[1] == 'time':
-                # либо считать среднее, либо выбирать НаИбОлЬшЕе (_найти движок, который нормально работает)
-                if 'depth' in temp:
-                    if out_max_res < int(temp[9]):
-                        out_max_res = int(temp[9])
-                else:
-                    if out_max_res < int(temp[7]):
-                        out_max_res = int(temp[7])
+            ## зависит от движка
+            if len(temp) > 19:
+                print(temp[14])
+                if out_max_res < int(temp[15]):
+                    out_max_res = int(temp[15])
+            # if temp[1] == 'time':
+            #     # либо считать среднее, либо выбирать НаИбОлЬшЕе
+            #     if 'depth' in temp:
+            #         if out_max_res < int(temp[9]):
+            #             out_max_res = int(temp[9])
+            #     else:
+            #         if out_max_res < int(temp[7]):
+            #             out_max_res = int(temp[7])
 
     # найти cp за лучший следующий ход
     start_pos += ' ' + kif['moves'][i]
     temp_str = 'position startpos moves ' + start_pos + '\r\n'
-    print(temp_str[:-2], " ход........ ", end="")
+    print(temp_str[:-2], " лучший следующий ход........ ", end="")
     process.stdin.write((temp_str).encode())
     process.stdin.flush()
     process.stdin.write('go infinite\r\n'.encode())
@@ -88,15 +102,19 @@ for i in range(len(kif['moves'])):
     out_variants = {}
     while line_end := process.stdout.readline().decode('utf8'):
         if line_end.find('bestmove') > -1:
-            print(line_end.replace('\r\n',''), end='')
-            print('///',out_variants[line_end.split(' ')[-1].replace('\r\n','')])
+            print(line_end.replace('\r\n',''), end=' ')
+            print('///',out_variants[line_end.split(' ')[1].replace('\r\n','')])
             break
         else:
             temp = line_end.split(' ')
-            if temp[1] == 'time':
-                # либо считать среднее, либо выбирать наибольшее
-                if 'depth' in temp:
-                    out_variants[temp[11].replace('\r\n','')] = temp[9]
-                else:
-                    out_variants[temp[11].replace('\r\n','')] = temp[7]
+            ## зависит от движка
+            if len(temp) > 19:
+                print(temp[14],temp[15],temp[18],temp[19])
+                out_variants[temp[19].replace('\r\n','')] = temp[15]
+            # if temp[1] == 'time':
+            #     # либо считать среднее, либо выбирать наибольшее
+            #     if 'depth' in temp:
+            #         out_variants[temp[11].replace('\r\n','')] = temp[9]
+            #     else:
+            #         out_variants[temp[11].replace('\r\n','')] = temp[7]
 process.kill()

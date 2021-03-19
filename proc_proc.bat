f9a3b292a44f47377e8f5d3d040993bce8f9cc4c
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-03-19 22:50:21 +0500

    нужно что-то вроде start, но вроде не start

--- proc_proc.bat
+++ proc_proc.bat
@@ -7,14 +7,10 @@ set counter=0
 for /f "delims=" %%i in (input.txt) do (
 	echo %%i>>temp_input.txt
 	set /a counter=!counter!+1
-	echo !counter! >> 2121212.txt
-	if !counter! equ 3 goto next
-
+	if !counter! geq 3 (
 	set counter=0
 	C:\Users\Olga\Downloads\gikou2_win\gikou<temp_input.txt
-	ping -n 1 -w 100000 192.168.254.254 >nul
-
-	:next
-	echo >temp_input.txt
+	ping -n 1 -w 10000 192.168.254.254 >nul
+	del temp_input.txt
+	)
 )
-echo counter=%counter%

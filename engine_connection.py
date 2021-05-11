04072749451ec8342b18dc9c83ed3d389ee5588d
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-05-11 16:29:43 +0500

    замер времени при ограничении на глубину

--- engine_connection.py
+++ engine_connection.py
@@ -28,6 +28,9 @@ class Connector:
 
 class Engine:
     def __init__(self,eng_adress):
+        self.Adress = eng_adress
+        self.Time_for_test = 0
+        self.Time_for_test_depth = 0
         self.process = subprocess.Popen(eng_adress, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         self.con = Connector()
@@ -37,10 +40,21 @@ class Engine:
         # process.stdin.write('setoption name WriteDebugLog value true\r\n'.encode())
         self.con.send_command_without_output(self.process,"usinewgame")
     
-    def make_move(self,moves_order,a,b,c):
+    def get_adress(self):
+        return self.Adress
+    
+    def get_time_for_test(self):
+        return self.Time_for_test
+
+    def get_time_for_test_depth(self):
+        return self.Time_for_test_depth
+
+    def make_move(self,moves_order,a,b,c):        
         self.con.send_command_without_output(self.process,"position startpos moves " + ' '.join(moves_order))
         temp_str = "go btime {0} wtime {1} byoyomi {2}".format(a,b,c)
+        temp_time = time.time()
         out = self.con.send_command_bestmove(self.process,temp_str)
+        self.Time_for_test += time.time() - temp_time
         return out
         
     def not_end(self,res):
@@ -51,49 +65,111 @@ class Engine:
         self.process.kill()
         self.process.wait()
 
+    def make_certain_depth_move(self,moves_order,depth):        
+        self.con.send_command_without_output(self.process,"position startpos moves " + ' '.join(moves_order))
+        temp_str = "go depth {0} btime {1} wtime {1} byoyomi {2}".format(depth,0,100000000)
+        temp_time = time.time()
+        out = self.con.send_command_bestmove(self.process,temp_str)
+        self.Time_for_test_depth += time.time() - temp_time
+        return out
 
 if __name__ == '__main__':
+    time_of_work = []
     f = open('new_output.txt', 'a')
+    engine_list = ["gikou","Kristallweizen-wcsc29-avx2","YaneuraOu_KPPT-tournament-clang++-avx2","nozomi"]
+    eng_time_param = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 
-    eng1 = Engine("gikou")
-    eng2 = Engine("YaneuraOu_KPPT-tournament-clang++-avx2")
+    for depth_counter in range(10):
+        for k1 in range(4):
+            for k2 in range(k1+1,4):
+                print(k1,k2)
+                eng1 = Engine(engine_list[k1])
+                eng2 = Engine(engine_list[k2])
 
-    for i in range(10):
-        moves_order = []
+                for i in range(10):
+                    moves_order = []
+                    while True and len(moves_order) < 400:
+                        move = eng1.make_certain_depth_move(moves_order,depth_counter+1)
+                        if move == "resign":
+                            f.write(eng1.get_adress() + " defeat " + eng2.get_adress() + "\n")
+                            eng1.not_end("win")
+                            eng2.not_end("lose")
+                            break
+                        moves_order.append(move)
+                        
+                        move = eng2.make_certain_depth_move(moves_order,depth_counter+1)
+                        if move == "resign":
+                            f.write(eng2.get_adress() + " defeat " + eng1.get_adress() + "\n") 
+                            eng1.not_end("lose")
+                            eng2.not_end("win")
+                            break
+                        moves_order.append(move)
+                    print("End")
+                if len(moves_order)<400:
+                    eng_time_param[k1][depth_counter] += eng1.get_time_for_test_depth()
+                    eng_time_param[k2][depth_counter] += eng2.get_time_for_test_depth()
+                else:
+                    f.write("stalemate " + eng1.get_adress() + " , " + eng2.get_adress() + "\n")
+                eng1.end()
+                eng2.end()
+for i in range(4):
+    f.write(str(i)+"\n")
+    for j in range(10):
+        f.write(str(float('{:.3f}'.format(eng_time_param[i][j]))) + "\n")
+f.close()
 
-        # a = 300000
-        # b = 300000
-        a = 0
-        b = 0
-        c = 100
-        while True:
-            start_time = time.time()
-            move = eng1.make_move(moves_order,a,b,c)
-            dt = time.time() - start_time
-            a -= dt
-            if move == "resign":
-                f.write("eng1 win\n")
-                print('')
-                eng1.not_end("win")
-                eng2.not_end("lose")
-                break
-            moves_order.append(move)
-            print(moves_order[-1], end=" ")
-            
-            start_time = time.time()
-            move = eng2.make_move(moves_order,a,b,c)
-            dt = time.time() - start_time
-            b -= dt
-            if move == "resign":
-                f.write("eng2 win\n")
-                print('')            
-                eng1.not_end("lose")
-                eng2.not_end("win")
-                break
-            moves_order.append(move)
+# if __name__ == '__main__':
+#     time_of_work = []
+#     f = open('new_output.txt', 'a')
+#     engine_list = ["gikou","Kristallweizen-wcsc29-avx2","YaneuraOu_KPPT-tournament-clang++-avx2","nozomi"]
+#     eng_time_param = [0,0,0,0]
 
-            print(moves_order[-1], end=" ")
-        print("End")
-    eng1.end()
-    eng2.end()
-    f.close()
+#     for k1 in range(4):
+#         for k2 in range(k1+1,4):
+#             print(k1,k2)
+#             eng1 = Engine(engine_list[k1])
+#             eng2 = Engine(engine_list[k2])
+
+#             for i in range(3):
+#                 moves_order = []
+
+#                 # a = 300000
+#                 # b = 300000
+#                 a = 0
+#                 b = 0
+#                 c = 100
+#                 while True:
+#                     start_time = time.time()
+#                     move = eng1.make_move(moves_order,a,b,c)
+#                     dt = time.time() - start_time
+#                     a -= dt
+#                     if move == "resign":
+#                         f.write(eng1.get_adress() + " win\n")
+#                         print('')
+#                         eng1.not_end("win")
+#                         eng2.not_end("lose")
+#                         break
+#                     moves_order.append(move)
+#                     print(moves_order[-1], end=" ")
+                    
+#                     start_time = time.time()
+#                     move = eng2.make_move(moves_order,a,b,c)
+#                     dt = time.time() - start_time
+#                     b -= dt
+#                     if move == "resign":
+#                         f.write(eng2.get_adress() + " win\n")
+#                         print('')            
+#                         eng1.not_end("lose")
+#                         eng2.not_end("win")
+#                         break
+#                     moves_order.append(move)
+#                     print(moves_order[-1], end=" ")
+
+#                 print("End")
+#             eng_time_param[k1] += eng1.get_time_for_test()
+#             eng_time_param[k2] += eng2.get_time_for_test()
+#             eng1.end()
+#             eng2.end()
+# for i in range(4):
+#     f.write(str(i)+str(float('{:.3f}'.format(eng_time_param[i]))) + "\n")
+# f.close()

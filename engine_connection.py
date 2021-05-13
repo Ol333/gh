070571693f0c96c00e289d9fd229ed2a383bd0a6
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-05-13 11:05:48 +0500

    график

--- engine_connection.py
+++ engine_connection.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import matplotlib.pyplot as plt
 
 class Connector:
     def send_command(self, process, command, expectation):
@@ -74,12 +75,13 @@ class Engine:
         return out
 
 if __name__ == '__main__':
+    max_depth = 15
     time_of_work = []
-    f = open('new_output.txt', 'a')
+    stalemate_list = [[],[],[],[]]
     engine_list = ["gikou","Kristallweizen-wcsc29-avx2","YaneuraOu_KPPT-tournament-clang++-avx2","nozomi"]
-    eng_time_param = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+    eng_time_param = [[0]*max_depth, [0]*max_depth, [0]*max_depth, [0]*max_depth]
 
-    for depth_counter in range(10):
+    for depth_counter in range(max_depth):
         for k1 in range(4):
             for k2 in range(k1+1,4):
                 print(k1,k2)
@@ -88,10 +90,9 @@ if __name__ == '__main__':
 
                 for i in range(10):
                     moves_order = []
-                    while True and len(moves_order) < 400:
+                    while True and len(moves_order) < 320:
                         move = eng1.make_certain_depth_move(moves_order,depth_counter+1)
                         if move == "resign":
-                            f.write(eng1.get_adress() + " defeat " + eng2.get_adress() + "\n")
                             eng1.not_end("win")
                             eng2.not_end("lose")
                             break
@@ -99,24 +100,40 @@ if __name__ == '__main__':
                         
                         move = eng2.make_certain_depth_move(moves_order,depth_counter+1)
                         if move == "resign":
-                            f.write(eng2.get_adress() + " defeat " + eng1.get_adress() + "\n") 
                             eng1.not_end("lose")
                             eng2.not_end("win")
                             break
                         moves_order.append(move)
                     print("End")
-                if len(moves_order)<400:
+                if len(moves_order)<320:
                     eng_time_param[k1][depth_counter] += eng1.get_time_for_test_depth()
                     eng_time_param[k2][depth_counter] += eng2.get_time_for_test_depth()
                 else:
-                    f.write("stalemate " + eng1.get_adress() + " , " + eng2.get_adress() + "\n")
+                    stalemate_list[k1].append(depth_counter)
+                    stalemate_list[k2].append(depth_counter)
                 eng1.end()
                 eng2.end()
 for i in range(4):
-    f.write(str(i)+"\n")
-    for j in range(10):
-        f.write(str(float('{:.3f}'.format(eng_time_param[i][j]))) + "\n")
-f.close()
+    for j in range(max_depth):
+        count = max_depth
+        if j in stalemate_list[i]:
+            count -= stalemate_list[i].count(j)
+        eng_time_param[i][j] = eng_time_param[i][j] / count
+
+fig, ax = plt.subplots()
+
+ax.set_title("Важный график зависимости времени размышлений от глубины поиска хода", fontsize=16)
+ax.set_xlabel("время, потраченное движком на ходы в течение игры, с", fontsize=14)
+ax.set_ylabel("глубина поиска лучшего хода", fontsize=14)
+ax.grid(which="major",linewidth=1.2)
+ax.plot(range(1,11),eng_time_param[0],label="gikou")
+ax.plot(range(1,11),eng_time_param[1],label="Kristallweizen-wcsc29-avx2")
+ax.plot(range(1,11),eng_time_param[2],label="YaneuraOu_KPPT-tournament-clang++-avx2")
+ax.plot(range(1,11),eng_time_param[3],label="nozomi")
+ax.legend()
+ax.tick_params(which='major', length=10, width=1)
+
+plt.show()
 
 # if __name__ == '__main__':
 #     time_of_work = []

1bbe816fb9af09c466bbb8bb410136d45cf18be8
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-09-29 16:10:51 +0500

    Запуски для одного шага, пока не сойдется

--- data_processing.py
+++ data_processing.py
@@ -1,62 +1,69 @@
 import sys
-import time
-
+import math
+import numpy as np
 import shogi.KIF
 
 import rab_with_db as rwd
 import engine_connection as ec
 
+z_85 = 1.00
+e = 10
+
 if __name__ == "__main__":
     run_counter = int(sys.argv[1])
-    print(run_counter, type(run_counter))
-    pl_list = rwd.player_list() # зачем?..
-    # print(len(pl_list))
+    kf_list = rwd.table_list("Kifu")
+    # print(kf_list[0])
 
-    for i_player in range(run_counter,run_counter+1): #запускаем по одному человеку за запуск
-        # 66 yukitakahashi
-        pl_kif_list = rwd.players_kifu_list(i_player)
-        for i_kif in range(1): #pl_kif_list:
-        # l = rwd.players_kifu_list(66)[0]
-            l = pl_kif_list[i_kif]
+    # for i_kifu in range(run_counter,run_counter+1000):
+    for i_kifu in range(run_counter,run_counter+1):
+        print('запуск ',i_kifu,' кифу')
+        try:
+            kif = shogi.KIF.Parser.parse_str(kf_list[i_kifu][1])[0]
+        except Exception as ex:
+            f = open('wrong_kifu.txt', 'a')
+            f.write(str(i_kifu) + '\n') #вывести в файл номер неправильного кифу
+            f.close()
+        eng = ec.Engine("YaneuraOu_NNUE-tournament-clang++-avx2")
 
-            kif = shogi.KIF.Parser.parse_str(l)[0]
-            print(kif['names'][shogi.BLACK])
-            print(kif['names'][shogi.WHITE])
-            print(kif['moves']) # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-            print(kif['win'])
+        start_pos = ""
+        for i in range(len(kif['moves'])):
+        # for i in range(10):
+            n = 0
+            needed_n = 10
+            sample_bst_mov = []
+            sample_mov_cp = []
+            mov_cp_std = 0
+            while n <= needed_n:
+                # найти cp за лучший рекомендуемый следующий ход
+                temp_because_yaneoura_besit = eng.cp_of_next_move(start_pos, depth=17)
+                if temp_because_yaneoura_besit[1] == -111111111:
+                    bst_mov = temp_because_yaneoura_besit[0]
+                    mov_cp = eng.cp_of_current_move(start_pos, bst_mov, depth=17)
+                else:
+                    bst_mov,mov_cp = temp_because_yaneoura_besit
+                sample_bst_mov.append(bst_mov)
+                sample_mov_cp.append(mov_cp)
+                n += 1
+                if n == 10:
+                    mov_cp_std = np.array(sample_mov_cp).std()
+                if n == needed_n:
+                    temp_np_array = np.array(sample_mov_cp)
+                    needed_n = math.ceil(((z_85*mov_cp_std)/e)**2)
+                    print('изменили needed_n',z_85,mov_cp_std,e)
+                    mov_cp_std = temp_np_array.std()
+            mov_cp = np.array(sample_mov_cp).mean()
+            print(sample_mov_cp)
+            print(len(set(sample_bst_mov)),'разных ходов')
+            print("mov_cp",mov_cp)
+            print("needed_n",needed_n)
+            # bst_mov = найти ближайший mov_cp из sample_mov_cp и взять по индексу bst_mov ### а верно ли это вообще ?
+            # отсортировать копию и бинарным поиском...
 
-            l = l.split('\n')
-            date = l[1][5:]
-            conditions = (l[3][5:], l[4][4:])
-            print(date)
-            print(conditions)
-            ##date = datetime.datetime.strptime(date,"%Y/%m/%d")
-            
-            for j in range(10): # for j in range(10,20)
-                eng = ec.Engine("YaneuraOu_NNUE-tournament-clang++-avx2")
-                f = open('Y_output_{0}_{1}_{2}.txt'.format(pl_list[i_player],i_kif,j), 'w')
+            # # найти cp за текущий ход
+            # cur_res = eng.cp_of_current_move(start_pos, kif['moves'][i], depth=17)
 
-                start_time = time.time()
-                start_pos = ""
-                for i in range(len(kif['moves'])):
-                    # найти cp за лучший рекомендуемый следующий ход
-                    temp_because_yaneoura_besit = eng.cp_of_next_move(start_pos, depth=5)
-                    if temp_because_yaneoura_besit[1] == -111111111:
-                        bst_mov = temp_because_yaneoura_besit[0]
-                        mov_cp = eng.cp_of_current_move(start_pos, bst_mov, depth=5)
-                    else:
-                        bst_mov,mov_cp = temp_because_yaneoura_besit
-                    f.write(" /// " + str(bst_mov) + ' ' + str(mov_cp) + '\n') #вывести в бд
-                    # найти cp за текущий ход
-                    cur_res = eng.cp_of_current_move(start_pos, kif['moves'][i], depth=5)
-                    f.write(' '+ kif['moves'][i] + ' /// ' + str(cur_res) + '\n') #вывести в бд
-                    start_pos += ' ' + kif['moves'][i]
-                eng.end()
-                res_time = time.time() - start_time
-                res_min = str(res_time // 60)
-                res_sec = str(float('{:.3f}'.format(res_time % 60)))
-                f.write(res_min + " minutes, " + res_sec + " seconds")
-                f.close()
-
-
-# в процессе изменяется .db
+            # # записать в бд
+            # # провести больше запусков, чтобы получить число поточнее..................................................
+            # rwd.move_add(kf_list[i_kifu][0],i,mov_cp,bst_mov,cur_res)
+            start_pos += ' ' + kif['moves'][i]
+        eng.end()

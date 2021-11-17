f5c39c3ad20d494e21a6e128ece0aa91c108b7f6
Ol333 <36545044+Ol333@users.noreply.github.com>
2021-11-17 18:57:35 +0500

    проверка погрешности после попыток ее уменьшить в data_processing

--- graf_dp.py
+++ graf_dp.py
@@ -1,5 +1,70 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import math
+
+# if __name__ == '__main__':
+#     all_moves_real = pd.DataFrame()
+#     all_moves_pv = pd.DataFrame()
+#     fig, ax = plt.subplots()
+#     ax.set_title("Оценка ходов 666 партии движком", fontsize=16)
+#     ax.set_xlabel("Номер хода", fontsize=14)
+#     ax.set_ylabel("cp", fontsize=14)
+#     ax.minorticks_on()
+#     ax.grid(which="major",linewidth=1.2)
+#     ax.grid(which="minor",linestyle = ':')
+
+#     for i in range(1,20):
+#         f_name = "666/666_{}.txt".format(i)
+#         dot_mas_real = [[],[]]
+#         dot_mas_pv = [[],[]]
+
+#         f = open(f_name,'r')
+#         iterator = 0
+#         for line in f:
+#             line_mas = line.replace('\n','').split(' ')
+#             line_mas.remove(line_mas[0])
+#             line_mas.remove(line_mas[1])
+#             line_mas = list(map(int,line_mas))
+#             if iterator%2 != 0:
+#                 line_mas[1] = -line_mas[1]
+#                 line_mas[2] = -line_mas[2]
+#             dot_mas_pv[1].append(line_mas[1])
+#             dot_mas_pv[0].append(line_mas[0])
+#             dot_mas_real[1].append(line_mas[2])
+#             dot_mas_real[0].append(line_mas[0])
+#             iterator += 1
+
+#         f.close()
+#         ax.scatter(dot_mas_pv[0],dot_mas_pv[1], s=70, marker='*', color='g')
+#         ax.scatter(dot_mas_real[0],dot_mas_real[1], s=70, marker='*', color='b')
+#         all_moves_real[i] = dot_mas_real[1]
+#         all_moves_pv[i] = dot_mas_pv[1]
+#     plt.show()
+#     all_moves_real["std"] = list(np.array(all_moves_real.iloc[i]).std() for i in range(76))
+#     all_moves_pv["std"] = list(np.array(all_moves_pv.iloc[i]).std() for i in range(76))
+#     print("максимальное отклонение по реальным оценкам",max(all_moves_real["std"]))
+#     print("максимальное отклонение по предсказаниям",max(all_moves_pv["std"]))
+    
+#     all_moves_real["относительная погрешность"] = [0] * 76
+#     all_moves_pv["относительная погрешность"] = [0] * 76
+#     for i in range(76):
+#         move = np.array(all_moves_real.iloc[i])
+#         temp_mean = np.mean(move)
+#         del_2_rcp = sum(list(map(lambda x:(x-temp_mean)**2,move)))
+#         del_random = 2*math.sqrt(del_2_rcp/(19*18))
+#         all_moves_real.loc[i,"относительная погрешность"] = 100*del_random/abs(temp_mean)
+        
+#         move = np.array(all_moves_pv.iloc[i])
+#         temp_mean = np.mean(move)
+#         del_2_rcp = sum(list(map(lambda x:(x-temp_mean)**2,move)))
+#         del_random = 2*math.sqrt(del_2_rcp/(19*18))
+#         if temp_mean != 0:
+#             all_moves_pv.loc[i,"относительная погрешность"] = 100*del_random/abs(temp_mean)
+
+#     print("максимальная относительная погрешность по реальным оценкам",max(all_moves_real["относительная погрешность"]))
+#     print("максимальная относительная погрешность по предсказаниям",max(all_moves_pv["относительная погрешность"]))
+        
 
 if __name__ == '__main__':
     colors0 = ['b','r']
